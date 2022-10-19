85d7b779585abfbf7395a37c5f910133b5373c0a
Kehinde ojo <ojok761@gmail.com>
2022-10-19 13:57:49 -0400

    did some more excersies

---
+++ ifscripts/greeting.js
@@ -0,0 +1,15 @@
+var morning;
+var evening= 100;
+var day;
+var hours=(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23)
+if (morning <10 && morning < 23 ){
+    console.log("Good Morning")
+}else if (day >10 && day < 23){
+    console.log("Good day ")
+}else if (evening > 17 && evening < 23){
+    console.log("Good evening")
+}else {
+    console.log("invalid hour")
+    
+
+}
--- ifscripts/pay_rules.js
+++ ifscripts/pay_rules.js
@@ -2,3 +2,12 @@ var payRate = 12.50;
 var hoursWorked= 20;
 var grossPay =payRate * hoursWorked
 console.log(grossPay)
+
+if (hoursWorked > 40){
+    console.log("over 40 hours")
+} else if (hoursWorked == 40){
+    console.log("exactly 40 hours ")
+}else{
+    console.log("under 40 hours")
+
+}
