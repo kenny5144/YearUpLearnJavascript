488e06db8e4bd458b5449651d97ae82f50d01c3a
Kehinde ojo <ojok761@gmail.com>
2022-10-19 15:42:01 -0400

    did min max excersice

---
+++ ifscripts/min_max.js
@@ -0,0 +1,17 @@
+var a= 234;
+var b = 456;
+var c = 879;
+
+var max= b;
+if (b > max){
+    max = b;
+    console.log(max)
+}else if (a > max){
+    max = a;
+    console.log(max)
+
+}else if (c > max){
+    max = c;
+    console.log(max)
+
+}
